f6cef4c523ba97e526007d6d5e13eba813de614a
Byeanseojin <sjb12365@gmail.com>
2024-09-03 16:20:37 +0900

    RepositoryTest

--- src/main/java/org/example/board/entity/Board.java
+++ src/main/java/org/example/board/entity/Board.java
@@ -18,6 +18,6 @@ public class Board extends BaseEntity {
     private String title;
     private String content;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private Member writer; //Foreign key 설정(참조무결성 유지)
 }
