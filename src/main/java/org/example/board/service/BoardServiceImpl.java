86a9e46ea47ddfccba0e87cce9ae8bd9ea84112b
Byeanseojin <sjb12365@gmail.com>
2024-09-24 16:01:02 +0900

    하나의 원글 조회하는 기능 댓글과 함께 원글을 삭제 기능

--- src/main/java/org/example/board/service/BoardServiceImpl.java
+++ src/main/java/org/example/board/service/BoardServiceImpl.java
@@ -7,9 +7,11 @@ import org.example.board.dto.PageResultDTO;
 import org.example.board.entity.Board;
 import org.example.board.entity.Member;
 import org.example.board.repository.BoardRepository;
+import org.example.board.repository.ReplyRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.function.Function;
 
@@ -17,6 +19,7 @@ import java.util.function.Function;
 @RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService{
     private final BoardRepository repository;
+    private final ReplyRepository replyRepository;
     @Override
     public Long register(BoardDTO dto) {
         Board board = dtoToEntity(dto);
@@ -27,8 +30,28 @@ public class BoardServiceImpl implements BoardService{
 
     @Override
     public PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO) {
+
         Function<Object[], BoardDTO> fn = (en -> entityToDTO((Board) en[0], (Member) en[1], (Long) en[2]));
         Page<Object[]> result = repository.getBoardWithReplyCount(pageRequestDTO.getPageable(Sort.by("bno").descending()));
+
         return new PageResultDTO<>(result, fn);
+    }
+
+    @Override
+    public BoardDTO get(Long bno) {
+        Object result = repository.getBoardByBno(bno);
+
+        Object[] arr = (Object[]) result;
+
+        BoardDTO boardDTO = entityToDTO((Board) arr[0],(Member) arr[1],(Long) arr[2]);
+        return boardDTO;
+    }
+    @Transactional
+    @Override
+    public void removeWithReplies(Long bno) {
+        //댓글 삭제
+        replyRepository.deleteByBno(bno);
+        //원글 삭제
+        repository.deleteById(bno);
     }
 }
--- src/test/java/org/example/board/service/BoardServieTest.java
+++ src/test/java/org/example/board/service/BoardServieTest.java
@@ -1,6 +1,8 @@
 package org.example.board.service;
 
 import org.example.board.dto.BoardDTO;
+import org.example.board.dto.PageRequestDTO;
+import org.example.board.dto.PageResultDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,5 +22,28 @@ public class BoardServieTest {
                 .build();
         Long bno = boardService.register(dto);
         System.out.println("정상적으로 글이 저장되었습니다. : "+bno);
+    }
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO = new PageRequestDTO();
+
+        PageResultDTO<BoardDTO, Object[]> result = boardService.getList(pageRequestDTO);
+
+        for (BoardDTO boardDTO : result.getDtoList()){
+            System.out.println(boardDTO);
+        }
+    }
+
+    @Test void testGet(){
+        Long bno = 2L;
+
+        BoardDTO boardDTO = boardService.get(bno);
+
+        System.out.println(boardDTO);
+    }
+    @Test
+    public void testRemove(){
+        Long bno = 1L;
+        boardService.removeWithReplies(bno);
     }
 }
