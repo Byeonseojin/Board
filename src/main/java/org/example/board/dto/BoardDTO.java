0efbd50e0c426ae89595a69d3c86c48bcdcabe3d
Byeanseojin <sjb12365@gmail.com>
2024-09-24 14:21:21 +0900

    Boardservice에 entityToDTO추가

--- src/main/java/org/example/board/dto/BoardDTO.java
+++ src/main/java/org/example/board/dto/BoardDTO.java
@@ -14,6 +14,7 @@ public class BoardDTO {
     private String title;
     private String content;
     private String writerEmail;
+    private String writerName;
     private LocalDateTime regDate;
     private LocalDateTime modDate;
     private int replyCount;
--- src/main/java/org/example/board/service/BoardService.java
+++ src/main/java/org/example/board/service/BoardService.java
@@ -1,12 +1,33 @@
 package org.example.board.service;
 
 import org.example.board.dto.BoardDTO;
+import org.example.board.dto.PageRequestDTO;
+import org.example.board.dto.PageResultDTO;
 import org.example.board.entity.Board;
 import org.example.board.entity.Member;
 
 public interface BoardService {
     // 새글을 등록하는 기능
     Long register(BoardDTO dto);
+    //게시목록 처리 기능
+
+    PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO);
+    // Entity를 DTO로 변환하는 메소드
+    default BoardDTO entityToDTO(Board board, Member member, Long replyCount){
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(board.getBno())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .writerEmail(member.getEmail())
+                .writerName(member.getName())
+                .replyCount(replyCount.intValue())
+                .build();
+
+        return boardDTO;
+    }
+    // DTO를 Entity로 변환하는 메소드
     default Board dtoToEntity(BoardDTO dto){
         Member member = Member.builder()
                 .email(dto.getWriterEmail())
