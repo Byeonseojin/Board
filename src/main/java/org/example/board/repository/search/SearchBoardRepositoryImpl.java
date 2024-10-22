6b4d5218455a9651e192cc53764dd5a35df0a8a1
Byeanseojin <sjb12365@gmail.com>
2024-10-22 15:58:35 +0900

    JPQL(Querydsl)을 사용한 검색기능 완성

--- src/main/java/org/example/board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/org/example/board/repository/search/SearchBoardRepositoryImpl.java
@@ -2,7 +2,10 @@ package org.example.board.repository.search;
 
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
 import org.example.board.entity.Board;
@@ -10,67 +13,79 @@ import org.example.board.entity.QBoard;
 import org.example.board.entity.QMember;
 import org.example.board.entity.QReply;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Log4j2
 public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
+
     public SearchBoardRepositoryImpl(){
         super(Board.class);
     }
+
     @Override
-    public Board search1(){
+    public Board search1() {
         log.info("search1() 메소드 호출됨");
         QBoard board = QBoard.board;
         QReply reply = QReply.reply;
         QMember member = QMember.member;
 
+
+
         JPQLQuery<Board> jpqlQuery = from(board);
         jpqlQuery.leftJoin(member).on(board.writer.eq(member));
-        jpqlQuery.leftJoin(reply).on(reply.board.eq(board)).groupBy(board, reply);
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
 
-        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count()).groupBy(board, member, reply);
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count());
         tuple.groupBy(board, member, reply);
 
-        log.info("============================");
-        log.info(tuple);
-        log.info("============================");
+        log.info("=================================");
+        log.info(jpqlQuery);
+        log.info("=================================");
 
-        //JPQL 실행 방법
+        // JPQL 실행 방법
         List<Tuple> result = tuple.fetch();
-        log.info("============================");
+
+        log.info("=================================");
         log.info(result);
-        log.info("============================");
+        log.info("=================================");
+
+
         return null;
-
-
     }
 
     @Override
     public Page<Object[]> searchPage(String type, String keyword, Pageable pageable) {
         log.info("searchPage() 메소드 호출됨");
+
         QBoard board = QBoard.board;
         QReply reply = QReply.reply;
         QMember member = QMember.member;
 
+
+
         JPQLQuery<Board> jpqlQuery = from(board);
         jpqlQuery.leftJoin(member).on(board.writer.eq(member));
-        jpqlQuery.leftJoin(reply).on(reply.board.eq(board)).groupBy(board, reply);
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
 
-        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count()).groupBy(board, member, reply);
-        tuple.groupBy(board, member, reply);
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member, reply.count());
+//        tuple.groupBy(board, member, reply);
 
         BooleanBuilder booleanBuilder = new BooleanBuilder();
-        BooleanExpression expression = board.bno.gt(0L);
+        BooleanExpression expression =board.bno.gt(0L);
+
         booleanBuilder.and(expression);
 
         if(type != null){
             String[] typeArr = type.split("");
             BooleanBuilder conditionBuilder = new BooleanBuilder();
 
-            for(String t: typeArr){
+            for(String t:typeArr){
                 switch (t){
                     case "t":
                         conditionBuilder.or(board.title.contains(keyword));
@@ -81,14 +96,37 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
                     case "c":
                         conditionBuilder.or(board.content.contains(keyword));
                         break;
-                } //end switch
+                }//end switch
             }//end for
-            booleanBuilder.and(booleanBuilder);
+            booleanBuilder.and(conditionBuilder);
         }//end if
+
         tuple.where(booleanBuilder);
+        // Sorting(Order by)
+        Sort sort = pageable.getSort();
+
+        sort.stream().forEach(order -> {
+            Order direction = order.isAscending()? Order.ASC:Order.DESC;
+            String prop = order.getProperty();
+
+            PathBuilder orderByExpression = new PathBuilder(Board.class, "board");
+            tuple.orderBy(new OrderSpecifier(direction, orderByExpression.get(prop)));
+        });
+
         tuple.groupBy(board, member);
+
+        // 페이지 처리에 필요한 값(offset, limit) 설정
+        tuple.offset(pageable.getOffset());
+        tuple.limit(pageable.getPageSize());
+
         List<Tuple> result = tuple.fetch();
+
         log.info(result);
-        return null;
+
+        long count = tuple.fetchCount();
+
+        log.info("실행된 행의 개수: " + count);
+
+        return new PageImpl<Object[]>(result.stream().map(t -> t.toArray()).collect(Collectors.toList()), pageable, count);
     }
 }
--- src/test/java/org/example/board/service/BoardServiceTest.java
+++ src/test/java/org/example/board/service/BoardServiceTest.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class BoardServieTest {
+public class BoardServiceTest {
 
     @Autowired
     private BoardService boardService;
